b92d1e1156f4509c0599a3428ebc63fc130eaf42
dangkh <Dangkh109@gmail.com>
2021-08-02 00:46:43 +0700

    refactor task 1

--- NIP_task1.py
+++ NIP_task1.py
@@ -2,25 +2,26 @@ from ultis import *
 import argparse
 import numpy as np
 import torch
-import matplotlib.pyplot as plt
 from nets import *
+import nets
 import torch.optim as optim
 from torch.optim import lr_scheduler
-import argparse
-
+from modelUltis import *
+ 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Train task 1')
 	parser.add_argument('-e', type=int, default = 10, help = "Number of training epoch")
 	parser.add_argument('-lr', type=float, default = 0.0001, help = "Learning rate")
 	parser.add_argument('-v', type=bool, default = False, help = "Visualize data")
 	parser.add_argument('-net', type=str, default = "LSTMNet", help = "Model")
-	parser.add_argument('-loss', type=str, help = "Loss for model")
+	parser.add_argument('-loss', type=str, default = "CrossEntropyLoss", help = "Loss for model")
 	parser.add_argument('-optim', type=str, help = "Optimizer for model")
 	parser.add_argument('-valid', type=bool, help = "Check on valid fold")
 	parser.add_argument('-confusion', type=bool, help = "Confusion matrix")
-	parser.add_argument('-save', type=bool, help = "Save")
+	parser.add_argument('-save', type=bool,default = False, help = "Save")
 	parser.add_argument('-mp', type=str, default = "model.pkl", help = "Saving model path")
 	parser.add_argument('-info', type=bool, default = False, help = "Show data info")
+	parser.add_argument('-lognum', type=int, default = 100, help = "Number of training epoch")
 	args = parser.parse_args()
 	params = vars(args)
 	
@@ -44,114 +45,27 @@ if __name__ == "__main__":
 	# train model
 
 	num_class = len(np.unique(ys))
-	print(params['net'])
-	stop
-	# model = LSTMNet(n_classes=num_class)
-	# model.double()
-	# if torch.cuda.is_available():
-	#   model.cuda()
-	# criterion = nn.CrossEntropyLoss()
-	# lr = 1e-4
-	# optimizer = optim.Adam(model.parameters(), lr=lr)
-	# scheduler = lr_scheduler.StepLR(optimizer, 16, gamma=0.1, last_epoch=-1)
-	# n_epochs = 35
-	# log_batch = 200
-	# llos = []
-	# lacc = []
+	model = getattr(nets, params['net'])(n_classes=num_class)
+	model.double()
+	if torch.cuda.is_available():
+		model.cuda()
+	criterion = getattr(nn, params['loss'])()
+	lr = params['lr']
+	optimizer = getattr(optim, 'Adam')(model.parameters(), lr=lr)
+	scheduler = lr_scheduler.StepLR(optimizer, 16, gamma=0.1, last_epoch=-1)
+	n_epochs = params['e']
+
+	trainModel(model, criterion, n_epochs, optimizer, scheduler, trainLoader, params['save'], params['lognum'])
 
 
-	# for epoch in range(n_epochs):  # loop over the dataset multiple times
-	# 	model.train()
-	# 	print("epoch:     ", epoch)
-	# 	running_loss = 0.0
-	# 	total_loss = 0
-	# 	for i, data in enumerate(trainLoader):
-	# 		# get the inputs; data is a list of [inputs, labels]
-	# 		inputs, labels = data
-	# 		labels = labels.type(torch.LongTensor)
-	# 		# CUDA
-	# 		if torch.cuda.is_available():
-	# 			inputs = inputs.cuda()
-	# 			labels = labels.cuda()
-	# 		# zero the parameter gradients
-	# 		optimizer.zero_grad()
+	# eval model
+	evaluateModel(model, plotConfusion = True, dataLoader = trainLoader)
+	evaluateModel(model, plotConfusion = True, dataLoader = testLoader)
 
-	# 		# forward + backward + optimize
-	# 		outputs = model(inputs)
-	# 		loss = criterion(outputs, labels)
-	# 		loss.backward()
-	# 		optimizer.step()
+	# test the target
 
-	# 		# print statistics
-	# 		running_loss += loss.item()
-	# 		total_loss += loss.item()
-	# 		if i % log_batch == (log_batch - 1):    # print every 200 mini-batches
-	# 			print('[%d, %5d] loss: %.3f' %
-	# 					(epoch + 1, i + 1, running_loss / log_batch))
-	# 			running_loss = 0.0
-	# 	mean_loss = total_loss / len(trainLoader)
-	# 	llos.append(mean_loss)
-	# 	scheduler.step()
-	# 	counter = 0
-	# 	for idx, data in enumerate(testLoader):
-	# 		xx, yy = data
-	# 		if torch.cuda.is_available():
-	# 			xx = xx.cuda()
-	# 			yy = yy.cuda()
-	# 		with torch.no_grad():
-	# 			model.eval()
-	# 			pred = model(xx)
-	# 			res = torch.argmax(pred, 1)
-	# 			for i, ypred in  enumerate(res):
-	# 				if ypred == yy[i].item():
-	# 					counter += 1
-	# 	acc = counter / len(testLoader)
-	# 	lacc.append(acc)
-	# print('Finished Training')
-
-
-	# # plot info
-	# counter = 0
-	# total = 0
-	# preds = []
-	# trueLabel = []
-	# for idx, data in enumerate(testLoader):
-	# 	xx, yy = data
-	# 	trueLabel.extend(yy.numpy())
-	# 	total += len(yy)
-	# 	# cuda
-	# 	if torch.cuda.is_available():
-	# 		xx = xx.cuda()
-	# 	with torch.no_grad():
-	# 		model.eval()
-	# 		pred = model(xx)
-	# 		res = torch.argmax(pred, 1)
-	# 		if torch.cuda.is_available():
-	# 			res = res.cpu()
-	# 		preds.extend(res.numpy())
-	# 		for id, ypred in enumerate(res):
-	# 			if ypred == yy[id].item():
-	# 				counter += 1
-	# print('acc: {:1f}%'.format(100 * counter / total))
-	# # plotLossAcc(llos, lacc)
-	# plot_confusion_matrix(trueLabel, preds, classes=['0', '1', '2', '3', '4', '5'], 
- #                                      normalize=True, title='Validation confusion matrix')
-	# plt.show()
-	# # test the target
-	# Xtarget = getTargetData()
-	# finaldataset = EEG_data(Xtarget)
-	# finalLoader = torch.utils.data.DataLoader(dataset=finaldataset, batch_size= 32)
-	# counter = 0
-	# resTest = []
-	# for idx, data in enumerate(finalLoader):
-	# 	xx = data
-	# 	# cuda
-	# 	if torch.cuda.is_available():
-	# 		xx = xx.cuda()
-	# 	with torch.no_grad():
-	# 		model.eval()
-	# 		pred = model(xx)
-	# 		res = torch.argmax(pred, 1)
-	# 		resTest.extend(res.cpu().numpy())
-
-	# np.savetxt("./answer.txt",[resTest],delimiter=',', fmt = "%d")
+	Xtarget = getSleepTestData()
+	finaldataset = EEG_data(Xtarget)
+	testDataLoader = torch.utils.data.DataLoader(dataset=finaldataset, batch_size= 32)
+	res = testModel(model, testDataLoader)
+	np.savetxt("./answerTask1.txt",[res],delimiter=',', fmt = "%d")
--- nets.py
+++ nets.py
@@ -1,66 +1,104 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-    
+	
 class LSTMNet(nn.Module):
-    def __init__(self, n_classes):
-        super(LSTMNet, self).__init__()
-        self.drop1 = nn.Dropout(0.75)
-        self.fc = nn.Linear(1024, n_classes)
-        self.n_classes = n_classes
-        self.conv1 = nn.Conv2d(1, 32, [25, 2], stride=[2, 1], padding = [1, 1])
-        torch.nn.init.xavier_uniform_(self.conv1.weight, gain=1)
-        self.mp1 = nn.AvgPool2d([75,1], stride = [20, 1])
-        self.conv2 = nn.Conv2d(32, 64, [25, 1], stride=[3, 1])
-        self.bnorm = nn.BatchNorm2d(64, momentum=0.1, affine=True)
-        torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1)
-        self.mp2 = nn.AvgPool2d([10,1], stride = [5, 1])
-        self.lstm = nn.LSTM(3, 8, batch_first=True)
+	def __init__(self, n_classes):
+		super(LSTMNet, self).__init__()
+		self.drop1 = nn.Dropout(0.75)
+		self.fc = nn.Linear(1024, n_classes)
+		self.n_classes = n_classes
+		self.conv1 = nn.Conv2d(1, 32, [25, 2], stride=[2, 1], padding = [1, 1])
+		torch.nn.init.xavier_uniform_(self.conv1.weight, gain=1)
+		self.mp1 = nn.AvgPool2d([75,1], stride = [20, 1])
+		self.conv2 = nn.Conv2d(32, 64, [25, 1], stride=[3, 1])
+		self.bnorm = nn.BatchNorm2d(64, momentum=0.1, affine=True)
+		torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1)
+		self.mp2 = nn.AvgPool2d([10,1], stride = [5, 1])
+		self.lstm = nn.LSTM(3, 8, batch_first=True)
 
-    def forward(self, x):
-        output = self.conv1(x)
-        output =  F.relu(output)
-        output = self.mp1(output)
-        output = self.conv2(output)
-        output = self.bnorm(output)
-        output =  F.relu(output)
-        output = self.mp2(output)
-        shape = output.shape
-        preLSTM = output.view(len(output), shape[1] * shape[2], -1)
-        h_lstm, _ = self.lstm(preLSTM)
-        output = torch.flatten(h_lstm,1)
-        output = self.drop1(output)
-        scores = self.fc(output)
-        return scores
+	def forward(self, x):
+		output = self.conv1(x)
+		output =  F.relu(output)
+		output = self.mp1(output)
+		output = self.conv2(output)
+		output = self.bnorm(output)
+		output =  F.relu(output)
+		output = self.mp2(output)
+		shape = output.shape
+		preLSTM = output.view(len(output), shape[1] * shape[2], -1)
+		h_lstm, _ = self.lstm(preLSTM)
+		output = torch.flatten(h_lstm,1)
+		output = self.drop1(output)
+		scores = self.fc(output)
+		return scores
 
 
 class LSTMNet_t2(nn.Module):
-    def __init__(self, n_classes):
-        super(LSTMNet_t2, self).__init__()
-        self.drop1 = nn.Dropout(0.75)
-        self.fc = nn.Linear(5120, n_classes)
-        self.n_classes = n_classes
-        self.conv1 = nn.Conv2d(1, 32, [10, 3], stride=[3, 2], padding = [1, 1])
-        torch.nn.init.xavier_uniform_(self.conv1.weight, gain=1)
-        self.mp1 = nn.AvgPool2d([10,1], stride = [2, 1])
-        self.conv2 = nn.Conv2d(32, 64, [10, 2], stride=[2, 1])
-        torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1)
-        self.mp2 = nn.AvgPool2d([10,1], stride = [2, 1])
-        self.lstm = nn.LSTM(8, 16, batch_first=True)
+	def __init__(self, n_classes):
+		super(LSTMNet_t2, self).__init__()
+		self.drop1 = nn.Dropout(0.75)
+		self.fc = nn.Linear(5120, n_classes)
+		self.n_classes = n_classes
+		self.conv1 = nn.Conv2d(1, 32, [10, 3], stride=[3, 2], padding = [1, 1])
+		torch.nn.init.xavier_uniform_(self.conv1.weight, gain=1)
+		self.mp1 = nn.AvgPool2d([10,1], stride = [2, 1])
+		self.conv2 = nn.Conv2d(32, 64, [10, 2], stride=[2, 1])
+		torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1)
+		self.mp2 = nn.AvgPool2d([10,1], stride = [2, 1])
+		self.lstm = nn.LSTM(8, 16, batch_first=True)
 
-    def forward(self, x):
-        output = self.conv1(x)
-        output =  F.relu(output)
-        output = self.mp1(output)
-        output = self.conv2(output)
-        output =  F.relu(output)
-        output = self.mp2(output)
-        shape = output.shape
-        preLSTM = output.view(len(output), shape[1] * shape[2], -1)
-        h_lstm, _ = self.lstm(preLSTM)
-        output = torch.flatten(h_lstm,1)
-        output = self.drop1(output)
-        scores = self.fc(output)
-        # scores = self.sm(output)
-        # stop
-        return scores
+	def forward(self, x):
+		output = self.conv1(x)
+		output =  F.relu(output)
+		output = self.mp1(output)
+		output = self.conv2(output)
+		output =  F.relu(output)
+		output = self.mp2(output)
+		shape = output.shape
+		preLSTM = output.view(len(output), shape[1] * shape[2], -1)
+		h_lstm, _ = self.lstm(preLSTM)
+		output = torch.flatten(h_lstm,1)
+		output = self.drop1(output)
+		scores = self.fc(output)
+		# scores = self.sm(output)
+		# stop
+		return scores
+
+class LSTMNet_t1_65(nn.Module):
+	def __init__(self, n_classes):
+		super(LSTMNet_t1_65, self).__init__()
+		self.drop1 = nn.Dropout(0.75)
+		self.fc = nn.Linear(2048, n_classes)
+		self.n_classes = n_classes
+		self.conv1 = nn.Conv2d(1, 32, [10, 2], stride=[2, 1], padding = [1, 1])
+		torch.nn.init.xavier_uniform_(self.conv1.weight, gain=1)
+		self.mp1 = nn.AvgPool2d([10,1], stride = [2, 1])
+		self.conv2 = nn.Conv2d(32, 64, [10, 1], stride=[3, 1])
+		torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1)
+		self.mp2 = nn.AvgPool2d([10,1], stride = [3, 1])
+		self.conv3 = nn.Conv2d(64, 128, [16, 1], stride=[4, 1])
+		torch.nn.init.xavier_uniform_(self.conv3.weight, gain=1)
+		self.avg = nn.AvgPool2d([8,1])
+		self.lstm = nn.LSTM(3, 8, batch_first=True)
+		self.sm = nn.Softmax(dim=-1)
+
+	def forward(self, x):
+		output = self.conv1(x)
+		output =  F.relu(output)
+		output = self.mp1(output)
+		output = self.conv2(output)
+		output =  F.relu(output)
+		output = self.mp2(output)
+		output = self.conv3(output)
+		output =  F.relu(output)
+		output = self.avg(output)
+		shape = output.shape
+		preLSTM = output.view(len(output), shape[1] * shape[2], -1)
+		h_lstm, _ = self.lstm(preLSTM)
+		output = torch.flatten(h_lstm,1)
+		output = self.drop1(output)
+		scores = self.fc(output)
+		# scores = self.sm(output)
+		# stop
+		return scores
